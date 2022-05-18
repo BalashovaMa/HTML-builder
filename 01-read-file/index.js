067f1a5cb60c2f2f94576bc6af38d68a21445022
BalashovaMa <balashova.m.nd@gmail.com>
2022-05-18 12:29:58 +0300

    feat: add solution task 01-read-file (Wed May 18 12:29:58     2022)

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+const stream = new fs.ReadStream(filePath);
+stream.on('readable', function () {
+    let data = stream.read();
+    if (data !== null) console.log(data.toString());
+});
