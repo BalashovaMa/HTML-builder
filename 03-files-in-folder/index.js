bea9a22be8a611e546ab53ca2af78105b188cb72
Maryna Balashova <balashova.m.nd@gmail.com>
2022-05-19 18:39:49 +0300

    fix: output file size (Thu May 19 18:39:49     2022)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,17 +1,25 @@
-const fs = require('fs');
-const path = require('path');
-const fsPromises = require('fs/promises');
-const folderPath = path.join(__dirname, 'secret-folder');
-fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
-    if (err)
-        console.log(err);
-    else {
-        files.forEach( file => {
-            const filePath = path.join(folderPath, file.name);
-            const data = fsPromises.stat(filePath);
-            console.log((file.name).split('.')[0] + ' - ' + path.extname(file.name).slice(1)
-                + ' - ' + data.size);
+const fs = require("fs");
+const path = require("path");
+const folderPath = path.join(__dirname, "secret-folder");
 
-        })
-    }
-})
+fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+        if (file.isFile()){
+            const filePath = path.join(folderPath, file.name);
+      fs.stat(filePath, function (err, stats) {
+        console.log(
+          file.name.split(".")[0] +
+            " - " +
+            path.extname(file.name).split(".")[1] +
+            " - " +
+            stats.size/1000 +
+            " kb"
+        );
+      });
+        }
+      
+    });
+  }
+});
