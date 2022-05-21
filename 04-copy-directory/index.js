12b5b27b8094f76cc70c4f4444c551a4aa52008e
Maryna Balashova <balashova.m.nd@gmail.com>
2022-05-21 21:17:33 +0300

    feat: add soulitoin 04-copy-folder (05/21/2022 21:17:33)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const { rm, mkdir, readdir, copyFile } = require("fs");
+const path = require("path");
+const sourcePath = path.join(__dirname, "files");
+const destinationPath = path.join(__dirname, "files-copy");
 
+const copyDir = (sourcePath, destinationPath) => {
+  rm(destinationPath, { recursive: true }, (err) => {
+    mkdir(destinationPath, { recursive: true }, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+    });
+    readdir(sourcePath, { withFileTypes: true }, (err, files) => {
+      if (err) {
+        return console.error(err);
+      }
+      files.forEach((file) => {
+        const fromCopy = path.join(sourcePath, file.name);
+        const toCopy = path.join(destinationPath, file.name);
+        if (file.isFile()) {
+          copyFile(fromCopy, toCopy, (err) => {
+            if (err) {
+              return console.error(err);
+            }
+          });
+        }
+        if (file.isDirectory()) {
+          copyDir(fromCopy, toCopy);
+        }
+      });
+    });
+  });
+};
+
+copyDir(sourcePath, destinationPath);
